760bab38110e0a240ae0ccce7ad8d4963534dc66
MlGIS <794237597@qq.com>
2025-04-05 09:21:45 +0800

    清空所有代码

--- src/main/java/com/skygeo/security/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.skygeo.security.repository;
-
-import com.skygeo.security.entity.User;
-import java.util.Optional;
-
-public interface UserRepository {
-    Optional<User> findByUsername(String username);
-    User save(User user);
-}
